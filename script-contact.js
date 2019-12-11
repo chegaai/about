23aeef5a769cb55ae01e52562acea488ec00f066
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2019-12-11 15:43:43 -0300

    Add contact page

---
+++ script-contact.js
@@ -0,0 +1,46 @@
+/**
+ * Returns a single item from an array randomly
+ * @param {Array} arr The array to extract the random item from
+ */
+function getRandomLogo (isFavicon = false) {
+  const lastResultKey = isFavicon ? 'last-favicon' : 'last-logo'
+  const lastResult = localStorage.getItem(lastResultKey)
+
+  const logoList = [
+    'mista',
+    'circulo',
+    'palito',
+    'retangulo',
+    'triangulo'
+  ]
+
+  const random = Math.floor(Math.random() * logoList.length)
+
+  if (isFavicon && random === 0) return getRandomLogo(isFavicon)
+
+  const result = logoList[random]
+
+  if (result === lastResult) return getRandomLogo(isFavicon)
+
+  localStorage.setItem(lastResultKey, result)
+  return result
+}
+
+function setRandomLogo () {
+  const newLogo = getRandomLogo()
+  const newLogoPath = `./assets/logo/${newLogo}.svg`
+  const logo = document.getElementById('logo')
+  logo.setAttribute('src', newLogoPath)
+}
+
+function setRandomFavicon () {
+  const newFavicon = getRandomLogo(true)
+  const newFaviconPath = `./assets/favicon/${newFavicon}.png`
+  const favicon = document.getElementById('favicon')
+  favicon.setAttribute('href', newFaviconPath)
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setRandomLogo()
+  setRandomFavicon()
+});
